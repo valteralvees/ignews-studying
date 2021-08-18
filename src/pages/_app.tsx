8cb0a67a3f14e599602b2ffb1f28b6e1323c9bf7
Valter <valteralvees@outlook.com>
2021-08-18 14:40:23 -0300

    Componente Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
-import { AppProps } from 'next/app'
+import { AppProps } from 'next/app';
+import { Header } from '../components/Header';
+
+import '../styles/global.scss'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return ( 
+    <>
+      <Component {...pageProps} />
+      <Header/>
+    </>
+  );
 }
 
 export default MyApp
