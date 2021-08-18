2e914d57573f07d8bef7761d090c3573f8845890
Valter <valteralvees@outlook.com>
2021-08-18 15:19:03 -0300

    Componente SignInButton

---
+++ src/components/SigInButton/index.tsx
@@ -0,0 +1,28 @@
+import { FaGithub } from 'react-icons/fa'
+import styles from './styles.module.scss'
+import { FiX } from 'react-icons/fi'
+
+export function SignInButton() {
+  const isUserLoggedIn = false;
+  
+  return isUserLoggedIn ? (
+    <button 
+    type="button"
+    className={styles.signInButton}
+    >
+      <FaGithub color="#04d361"/>
+      Valter Alves
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button 
+    type="button"
+    className={styles.signInButton}
+    >
+      <FaGithub color="#eba417"/>
+      Sign In with Github
+    </button>
+  )
+    
+  
+}
