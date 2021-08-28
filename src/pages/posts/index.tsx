46d2d33bb44365d8119ee30fe89c6019477e8151
Valter <valteralvees@outlook.com>
2021-08-27 21:47:56 -0300

    Servindo posts dinamicamente

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,8 +3,20 @@ import styles from './styles.module.scss';
 import Prismic from '@prismicio/client'
 import { getPrismicClient } from '../../services/prismic'
 import { GetStaticProps } from 'next';
+import {RichText} from 'prismic-dom'
 
-export default function Post() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({posts}: PostsProps) {
   return(
     <>
       <Head>
@@ -13,21 +25,14 @@ export default function Post() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Navigation should be visible, not tucked away</strong>
-            <p>The mobile version of your site may have truncated navigation, and that’s okay. </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Navigation should be visible, not tucked away</strong>
-            <p>The mobile version of your site may have truncated navigation, and that’s okay. </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Navigation should be visible, not tucked away</strong>
-            <p>The mobile version of your site may have truncated navigation, and that’s okay. </p>
-          </a>
+
+          {posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt} </p>
+            </a>
+          )) }
         </div>
       </main>
     </>
@@ -43,8 +48,24 @@ export const getStaticProps: GetStaticProps = async () => {
     fetch:['post.title', 'post.content'],
     pageSize: 100,
   })
+  
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    };
+  });
+
 
   return {
-    props:{}
+    props:{
+      posts
+    }
   }
 }
